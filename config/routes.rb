eddc90d3b76a377037f20862f38c929f99b7f514
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-08-01 14:28:24 +0000

    [Finish]11-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  get 'books/new'
+  get '/top' => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

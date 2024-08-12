272b42d430e0776cb7487abe4efdc9ae7ff5d07b
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-08-12 04:05:20 +0000

    [Check]14

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
   
   def create
    @book = Book.new(book_params)
@@ -32,15 +29,15 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
   
-  def destory
-    book = book.find(params[:id])
-    book.destory
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to '/books'
   end
   
   private
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
   
 end

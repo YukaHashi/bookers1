e775f2c62c3f3e8f25e8317a63a111dbce387b48
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-08-15 16:24:51 +0000

    test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-     flash[:notice] = "Book was succerrfully created."
+     flash[:notice] = "Book was successfully created."
      redirect_to book_path(@book.id)
     else
      flash.now[:alert] = "errors prohibited this book from being saved:"
